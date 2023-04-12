7c554299ad45e3299e30d7e36d6e635e6438998a
Guilherme Fernandes <guifernandespro@gmail.com>
2023-04-12 12:45:57 -0400

    implementing scss into project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './styles/styles.scss'
 
 import IndecisionApp from "./components/IndecisionApp";
-
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById("app"));
